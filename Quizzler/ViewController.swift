cf0fda6f27b7509ef50a5df4f0d378cb66394616
Sonali Marlin <sonali.marlin@techtonic.com>
2019-10-07 10:43:26 -0600

    add all files for Quizzler app

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,44 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    var questionNumber: Int = 0
+    let questionBank = QuestionBank()
+    var chosenAnswer: Bool = false
+    var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateUI()
+        
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            chosenAnswer = true
+        } else {
+            chosenAnswer = false
+        }
+        checkAnswer()
+        questionNumber += 1
+        updateUI()
     }
-    
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1 )
+        progressLabel.text = "\(questionNumber + 1)/ \(questionBank.list.count)"
+        nextQuestion()
     }
-    
 
     func nextQuestion() {
-        
+        if questionNumber >= questionBank.list.count {
+            let alert = UIAlertController(title: "Redo?", message: "You finished the quix. Would you like to retake it?", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Retake Quiz", style: .default, handler: { UIAlertAction in
+                self.startOver()
+            })
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            questionLabel.text = questionBank.list[questionNumber].questionText
+        }
     }
     
-    
     func checkAnswer() {
-        
+        let correctAnswer = questionBank.list[questionNumber].answer
+        if chosenAnswer == correctAnswer {
+            score += 1
+            ProgressHUD.showSuccess("Correct!")
+        } else {
+            ProgressHUD.showError("Incorrect!")
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        updateUI()
     }
-    
-
     
 }
